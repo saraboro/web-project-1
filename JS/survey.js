971333c3dfc254e614dee14d25abb02c7d2334a5
tamarcohen0509 <54308316+tamarcohen0509@users.noreply.github.com>
2019-08-20 13:34:33 +0300

    upload files
    Sari - there is a problem with the Nav-bar in survey.html - can you fix it?

---
+++ JS/survey.js
@@ -0,0 +1,17 @@
+/*
+============================================================================
+ survey.js
+ Web Project
+ Author: Tamar Cohen & Sara Borowski
+ ============================================================================
+*/
+
+function hide_continuous_car_accident_questions(elm)
+{
+	document.getElementById('continuous_car_accident_questions').style.display = "none";
+}
+
+function show_continuous_car_accident_questions(lm)
+{
+	document.getElementById('continuous_car_accident_questions').style.display = "block";
+}
